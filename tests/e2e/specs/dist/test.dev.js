30e38925a0177a34de7b453348bba195284fc249
Spencehiko <hikmet.kufteoglu@gmail.com>
2022-02-09 23:38:39 +0300

    basic e2e tests

---
+++ tests/e2e/specs/dist/test.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// https://docs.cypress.io/api/introduction/api.html
+describe("Whole Page Tests", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  it("All elements at the correct position", function () {
+    cy.get(".next-button").should("be.visible");
+    cy.get(".prev-button").should("not.be.visible");
+    cy.get(".upcoming-matches").should("be.visible").contains("h3", "Upcoming Matches");
+    cy.get(".date").should("be.visible").contains("/");
+    cy.get(".versus").should("be.visible");
+    cy.get(".abbr").should("be.visible");
+    cy.get(".score").should("be.visible");
+    cy.get(".status").should("be.visible").contains("ET");
+  });
+  it("Next button scrolls to right", function () {
+    cy.get(".next-button").click();
+    cy.get(".prev-button").should("be.visible");
+    cy.get(".card-wrapper").then(function (objCanvas) {
+      var width = objCanvas[0].offsetWidth;
+      var scrollAmount = (width / 175 - 1) * 175;
+      cy.get(".card-wrapper").should("have.attr", "style").should("contain", "translateX(-" + scrollAmount + "px)");
+    });
+  });
+});
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,41 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
-  })
-})
+describe("Whole Page Tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("All elements at the correct position", () => {
+    cy.get(".next-button")
+      .should("be.visible");
+    cy.get(".prev-button")
+      .should("not.be.visible");
+    cy.get(".upcoming-matches")
+      .should("be.visible")
+      .contains("h3", "Upcoming Matches");
+    cy.get(".date")
+      .should("be.visible")
+      .contains("/");
+    cy.get(".versus")
+      .should("be.visible");
+    cy.get(".abbr")
+      .should("be.visible");
+    cy.get(".score")
+      .should("be.visible");
+    cy.get(".status")
+      .should("be.visible")
+      .contains("ET");
+  });
+  it("Next button scrolls to right", () => {
+    cy.get(".next-button")
+      .click();
+    cy.get(".prev-button")
+      .should("be.visible");
+    cy.get(".card-wrapper").then((objCanvas) => {
+      const width = objCanvas[0].offsetWidth;
+      const scrollAmount = (width / 175 - 1) * 175;
+      cy.get(".card-wrapper")
+        .should("have.attr", "style")
+        .should("contain", "translateX(-" + scrollAmount + "px)");
+    });
+  });
+});
